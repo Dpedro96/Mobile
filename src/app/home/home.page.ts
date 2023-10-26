1cdbf0bc222a08369f5c83eb7b2812b8007f44e5
Dpedro96 <pedrohferreira300402@gmail.com>
2023-10-26 10:33:11 -0300

    'first'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  visor : string = '0';
+  valor1 : number = 0;
+  valor2 : number = 0;
+  operacao! : number;
   constructor() {}
 
+  adicionarNumero(valor : string){
+    if(this.visor.length == 1 && this.visor == '0'){
+      this.visor = valor;
+    }
+    else{
+      this.visor += valor;
+    }
+  }
+
+  limpar(){
+    this.visor = '0';
+  }
+
+  adicionarOperacao( valor : number){
+    this.operacao = valor;
+    this.valor1 = Number(this.visor);
+    this.limpar()
+  }
+
+  porcento(){
+    if(this.valor1 == 0){
+      this.valor2 = (Number(this.visor)/100);
+      this.visor = "" + this.valor2;
+    }
+    else{
+      this.valor2 = ((Number(this.visor)/100)*this.valor1);
+      this.visor = "" + this.valor2;
+      this.ingual();
+    }
+  }
+
+  trocaSinal(){
+    this.visor = "" + (Number(this.visor) * -1)
+  }
+
+  ingual(){
+    this.valor2 = Number(this.visor);
+    switch(this.operacao){
+      case 1 :{
+        this.visor = ""+(this.valor1 + this.valor2);
+        break;
+      }    
+      case 2 :{
+        this.visor =""+(this.valor1 - this.valor2);
+        break;
+      }   
+      case 3 :{
+        this.visor = "" +(this.valor1 * this.valor2);
+        break;
+      }  
+      case 4 :{
+        this.visor = "" +(this.valor1/this.valor2);
+        break;
+      }         
+    }
+    this.valor1=0;
+    this.valor2=0;
+    this.operacao=0;
+  }
 }
